1c1eb635d85cc5e8d1b992b3e75866a766391100
Ghaniya khan <165418041+Ghaniya08@users.noreply.github.com>
2024-10-07 00:13:23 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 import Hero from "@/components/hero";
 import Services from "@/components/services";
 import Deals from "@/components/deals";
 import Catogries from "@/components/catogries";
 import Foryou from "@/components/Foryou";
-import Successfulyymess from "@/components/Successfulyymess";
 export default function Home() {
   return (
     <div>
